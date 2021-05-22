8536f523a5c71c2d2b44ac3561b9770746db8518
Alexandra Roșca <71495113+alexandra-rosca@users.noreply.github.com>
2021-05-22 10:12:50 +0300

    Update Start.java

--- src/main/java/start/Start.java
+++ src/main/java/start/Start.java
@@ -1,13 +1,7 @@
 package start;
 
-import businesslogic.ClientBLL;
-import businesslogic.ProductBLL;
-import model.Client;
-import model.Product;
 import presentation.Controller;
 import presentation.GUI;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * The main class
@@ -18,26 +12,5 @@ public class Start {
     public static void main(String[] args) {
         GUI gui = new GUI();
         Controller controller = new Controller(gui);
-
-      /*  ClientBLL clientBLL = new ClientBLL();
-        Client client1 = null;
-        try{
-            client1 = clientBLL.findClient(1);
-            System.out.println(client1.toString());
-
-        } catch (Exception ex) {
-            LOGGER.log(Level.INFO, ex.getMessage());
-        }
-
-        ProductBLL productBLL = new ProductBLL();
-        Product product = null;
-        Product p = null;
-        try {
-           // product = productBLL.findById(1);
-            product = productBLL.findProduct("Oua" , "productName");
-            System.out.println(product.toString());
-        } catch (Exception ex) {
-            LOGGER.log(Level.INFO, ex.getMessage());
-        } */
     }
 }
